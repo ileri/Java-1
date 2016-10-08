7a5dc3ddc96b1137835fabcfa8ba9af5e279c2b2
Serhat Celil İLERİ <celil.ileri@bil.omu.edu.tr>
2016-10-09 02:13:20 +0300

    Create ArgumanTopla.java

---
+++ arguman_topla/ArgumanTopla.java
@@ -0,0 +1,10 @@
+public class ArgumanTopla
+{
+	public static void main(String args[])
+	{
+		int sayi1 = Integer.parseInt(args[0]);
+		int sayi2 = Integer.parseInt(args[1]);
+		int toplam = sayi1 + sayi2;
+		System.out.println(sayi1+" ve "+sayi2+" sayilarinin toplami: "+toplam);
+	}
+}
