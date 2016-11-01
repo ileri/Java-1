244d0e9bff69d1758137620b6321f33b22ee02e3
ebubekirdgn <ebubekir.dogan@bil.omu.edu.tr>
2016-11-01 23:06:36 +0300

    While ve Switch

---
+++ while-do-while-switch islemler/java4.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+public class java4 
+{
+
+	public static void main(String args[])
+	{
+		/*
+			Scanner ay = new Scanner (System.in);
+			System.out.println("Bir Ay Giriniz:");
+			int ay_Adi = ay.nextInt();
+		*//*
+		String sayi= JOptionPane.showInputDialog(null,"Merak ettiðiniz ay numarasýný giriniz :");
+		
+		int aynumarasi = Integer.parseInt(sayi);
+		
+		switch (aynumarasi) 
+		{
+		case 1:JOptionPane.showMessageDialog(null,"Ocak","Ay Bilgisi",JOptionPane.INFORMATION_MESSAGE);break;
+		case 2:JOptionPane.showMessageDialog(null,"Þubat","Ay Bilgisi",JOptionPane.INFORMATION_MESSAGE);break;
+		case 3:JOptionPane.showMessageDialog(null,"Mart","Ay Bilgisi",JOptionPane.INFORMATION_MESSAGE);break;
+		case 4:JOptionPane.showMessageDialog(null,"Nisan","Ay Bilgisi",JOptionPane.INFORMATION_MESSAGE);break;
+		case 5:JOptionPane.showMessageDialog(null,"Mayýs","Ay Bilgisi",JOptionPane.INFORMATION_MESSAGE);break;
+		case 6:JOptionPane.showMessageDialog(null,"Haziran","Ay Bilgisi",JOptionPane.INFORMATION_MESSAGE);break;
+		case 7:JOptionPane.showMessageDialog(null,"Temmuz","Ay Bilgisi",JOptionPane.INFORMATION_MESSAGE);break;
+		case 8:JOptionPane.showMessageDialog(null,"Aðustos","Ay Bilgisi",JOptionPane.INFORMATION_MESSAGE);break;
+		case 9:JOptionPane.showMessageDialog(null,"Eylül","Ay Bilgisi",JOptionPane.INFORMATION_MESSAGE);break;
+		case 10:JOptionPane.showMessageDialog(null,"Ekim","Ay Bilgisi",JOptionPane.INFORMATION_MESSAGE);break;
+		case 11:JOptionPane.showMessageDialog(null,"Kasým","Ay Bilgisi",JOptionPane.INFORMATION_MESSAGE);break;
+		case 12:JOptionPane.showMessageDialog(null,"Aralýk","Ay Bilgisi",JOptionPane.INFORMATION_MESSAGE);break;
+		
+		default:System.out.println("Bir yýl 12 aydan oluþmaktadýr.");break;
+		}
+		*/
+		
+		
+		int sayac = 1;
+		do {
+		 System.out.println("Sayaç: " + sayac);
+		 sayac++;
+		} while (sayac < 11);
+		sayac = 11;
+			while (sayac > 1)
+		    {
+			 System.out.println("Sayaç: " + sayac);
+			 sayac--;
+			} 
+    }
+}
