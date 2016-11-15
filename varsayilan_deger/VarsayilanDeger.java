2680c908b269e497f3caa88ae81baab5632b8cc4
celilileri <celil.ileri@bil.omu.edu.tr>
2016-11-15 20:34:31 +0300

    Varsayılan Değerler

---
+++ varsayilan_deger/VarsayilanDeger.java
@@ -0,0 +1,14 @@
+public class VarsayilanDeger{
+	public static void main(String args[]){
+		varsayilan a = new varsayilan();
+		System.out.println("Varsayılan byte:" + a.ilk_byte);
+		System.out.println("Varsayılan short:" + a.ilk_short);
+		System.out.println("Varsayılan int:" + a.ilk_int);
+		System.out.println("Varsayılan long:" + a.ilk_long);
+		System.out.println("Varsayılan float:" + a.ilk_float);
+		System.out.println("Varsayılan double:" + a.ilk_double);
+		System.out.println("Varsayılan char:" + a.ilk_char);
+		System.out.println("Varsayılan boolean:" + a.ilk_boolean);
+
+	}
+}
---
+++ varsayilan_deger/varsayilan.java
@@ -0,0 +1,10 @@
+public class varsayilan{
+	byte ilk_byte;
+	short ilk_short;
+	int ilk_int;
+	long ilk_long;
+	float ilk_float;
+	double ilk_double;
+	char ilk_char;
+	boolean ilk_boolean;
+}
