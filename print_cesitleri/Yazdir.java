2b21ed2d4c6dc74c61a69c7f5a97887346bd5b34
celilileri <celil.ileri@bil.omu.edu.tr>
2016-11-15 23:09:16 +0300

    Print çeşitleri

---
+++ print_cesitleri/Yazdir.java
@@ -0,0 +1,9 @@
+public class Yazdir{
+	public static void main(String args[]){
+		System.out.print("Merhaba Dünya\n");
+		System.out.printf("%d kere Merhaba Dünya\n", 10);
+		System.out.println("Yine yeni ve yeniden Merhaba Dünya");
+		System.out.printf("%5.2s\n","merhaba");
+		//5 karakterlik yer ayır, sağdan 2. karakterden itibaren yaz
+	}
+}
