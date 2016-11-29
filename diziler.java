6edd69857a22dd74c49e36533cac3a4f68f5d554
Ebubekir Doğan <ebubekir.dogan@bil.omu.edu.tr>
2016-11-29 21:21:40 +0200

    Add files via upload
    Diziler ve Çok Boyutlu Diziler

---
+++ diziler.java
@@ -0,0 +1,54 @@
+public class DiziDers {
+	public static void main(String [] args)
+	 {
+		
+///////////////////////////////////Normal Dizi//////////////////////////////////////////
+/* int dizi[] ={1,2,3,4,5};
+
+for(int i=0;i<5;i++)
+{
+System.out.println(dizi[i]);
+}*
+////////////////////////////////////Object Dizi//////////////////////////////////////////
+		 
+		 /*Object dizi [] = new Object[3];
+		 dizi[0]="Ebubekir";
+		 dizi[1]=2;
+		 dizi[2]=444.2;
+		 for(int i = 0;i<3;i++)
+		 {
+			 System.out.println((i+1)+ ". eleman = " + dizi[i]);
+		 } */
+	
+///////////////////////////////////////////////////////////////////////////// 
+		/*int liste [] = new int[5];
+		System.out.println("Dizinin ilk uzunlugu :" + liste.length);
+		liste = new int[15];
+		System.out.println("Dizinin yeni uzunlugu :" + liste.length);*/
+		
+/////////////////////////////////////////////////////////////////////////////
+		
+		/*double [] d ={1.2,2.2,3.2};
+		String [] s = {"defter","kalem"};
+		for(int i=0;i<d.length;i++)
+		{
+			System.out.println("d["+i+"] = " + d[i]);
+			System.out.println("s["+i+"] = " + s[i]); //HATA VERÝYOR.RUNTIME ERROR
+		}*/
+/////////////////////////////////////////////////////////////////////////////	
+		
+		/*int [][] dizi = {{1,2,3},{4,5,6}};
+		int [][] dizi2 = new int[3][4];
+		int [][] dizi3 = new int[3][];
+		int [][] dizi4 = new int[][4]; //hata veriyor
+		*/
+		//
+		
+		
+		
+		
+		
+		
+	 }
+	
+}
