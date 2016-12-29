7013eca44e682aed6f9ce27d6bf2dc5645f3d30a
celilileri <celil.ileri@bil.omu.edu.tr>
2016-12-29 13:13:25 +0300

    Boşa RAM israfı :D

---
+++ arrayRAM/dizi.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class dizi{
+	public static void main(String args[]){
+		LinkedList<String> liste = new LinkedList <String>();
+		for(long i = 0; i < 10000000 ; i++){
+			liste.add("Gereksiz RAM kullanımı :D");
+			liste.add("Gereksiz RAM kullanımı :D");
+		}
+	}
+}
